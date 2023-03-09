e136259f41e30ab7f6b4ddd9c891a6754f8602e1
parsasi <mail@parsa.pro>
2023-03-09 14:53:33 -0800

    Initial configuration is done

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import Image from "next/image";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Image src="/logo.png" width="120" height="50" alt="ORX API Logo" />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/ndcsol",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/ndcsol/orx-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: `ORX Travel API ${new Date().getFullYear()}`,
   },
-}
+  head: (
+    <>
+      <link rel="icon" href="/favicon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="ORX API Documentation" />
+    </>
+  ),
+  useNextSeoProps: () => {
+    return {
+      titleTemplate: "%s - ORX API",
+    };
+  },
+};
 
-export default config
+export default config;
